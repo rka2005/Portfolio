447be6b3093e3d600748c1b6887d2f25669133e0
Rohit Kumar Adak <rohitadak0@gmail.com>
2025-11-11 01:45:09 +0530

    Bugs fixed

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -89,10 +89,10 @@ export default function Contact() {
 
           <ul className="example-2">
             <li className="icon-content">
-              <a
-                data-social="linkedin"
-                aria-label="LinkedIn"
-                href="https://www.linkedin.com/in/rohit-adak-02b0342b3/"
+              <a 
+                data-social="email" 
+                aria-label="Email" 
+                href="mailto:rohitkr.adak0@example.com" 
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -100,19 +100,19 @@ export default function Contact() {
                 <svg
                   xmlSpace="preserve"
                   viewBox="0 0 16 16"
-                  className="bi bi-linkedin"
+                  className="bi bi-envelope"
                   fill="currentColor"
                   height="16"
                   width="16"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path
+                  <path 
                     fill="currentColor"
-                    d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"
+                    d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"
                   ></path>
                 </svg>
               </a>
-              <div className="tooltip">LinkedIn</div>
+              <div className="tooltip">Email</div>
             </li>
             <li className="icon-content">
               <a 
@@ -141,10 +141,10 @@ export default function Contact() {
               <div className="tooltip">GitHub</div>
             </li>
             <li className="icon-content">
-              <a 
-                data-social="email" 
-                aria-label="Email" 
-                href="mailto:rohitkr.adak0@example.com" 
+              <a
+                data-social="linkedin"
+                aria-label="LinkedIn"
+                href="https://www.linkedin.com/in/rohit-adak-02b0342b3/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -152,19 +152,19 @@ export default function Contact() {
                 <svg
                   xmlSpace="preserve"
                   viewBox="0 0 16 16"
-                  className="bi bi-envelope"
+                  className="bi bi-linkedin"
                   fill="currentColor"
                   height="16"
                   width="16"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path 
+                  <path
                     fill="currentColor"
-                    d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"
+                    d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"
                   ></path>
                 </svg>
               </a>
-              <div className="tooltip">Email</div>
+              <div className="tooltip">LinkedIn</div>
             </li>
             <li className="icon-content">
               <a
