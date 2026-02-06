0fbeb99b547c574bd097d168c2142b6398af45a6
ROHIT ADAK <rohitadak0@gmail.com>
2026-02-07 00:22:04 +0530

    new skills added

--- frontend/src/components/Skills.jsx
+++ frontend/src/components/Skills.jsx
@@ -3,13 +3,16 @@ import LogoLoop from '../components/LogoLoop';
 import { motion } from "framer-motion";
 
 const techLogos = [
+  { src: "/logos/c.svg", alt: "C" },
+  { src: "/logos/c++.svg", alt: "C++" },
+  { src: "/logos/java.svg", alt: "Java" },
+  { src: "/logos/python.svg", alt: "Python" },
+  { src: "/logos/django.svg", alt: "Django" },
   { src: "/logos/react.svg", alt: "React" },
   { src: "/logos/vite.svg", alt: "Vite" },
   { src: "/logos/javascript.svg", alt: "JavaScript" },
   { src: "/logos/typescript.svg", alt: "TypeScript" },
   { src: "/logos/tailwind.svg", alt: "Tailwind CSS" },
-  { src: "/logos/python.svg", alt: "Python" },
-  { src: "/logos/django.svg", alt: "Django" },
   { src: "/logos/firebase.svg", alt: "Firebase" },
   { src: "/logos/git.svg", alt: "Git" },
   { src: "/logos/github.svg", alt: "GitHub" },
@@ -17,6 +20,7 @@ const techLogos = [
   { src: "/logos/fastapi.svg", alt: "FastAPI" },
   { src: "/logos/mysql.svg", alt: "MySQL" },
   { src: "/logos/node-js.svg", alt: "NodeJS" },
+  { src: "/logos/express.svg", alt: "ExpressJS" },
 ];
 
 export default function Skills() {
@@ -34,7 +38,7 @@ export default function Skills() {
           pauseOnHover
           scaleOnHover
           fadeOut
-          fadeOutColor="#000000" 
+          fadeOutColor="#00000037" 
           ariaLabel="My technology skills"
         />
       </div>
